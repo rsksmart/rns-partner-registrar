ce70fae5258877a34a58c6b6843ad521284fe0b6
Onyeka Ijeh <onyekaijeh@gmail.com>
2022-11-01 15:57:55 +0100

    refactor: partner manager / access control

--- contracts/PartnerManager.sol
+++ contracts/PartnerManager.sol
@@ -1,31 +1,36 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.17;
 
-import "./RegistrarAccessControl.sol";
 import "./PartnerConfiguration.sol";
 import "./IPartnerManager.sol";
-import "./IRegistrarModule.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PartnerManager is IPartnerManager {
-    mapping(address => PartnerConfiguration) private _partnerConfigurations;
+contract PartnerManager is IPartnerManager, Ownable {
+    mapping(address => bool) private _partners;
 
-    IRegistrarModule private registrarModule;
+    constructor() Ownable() {}
 
-    constructor(IRegistrarModule _registrarModule) {
-        registrarModule = _registrarModule;
+    function isPartner(address partner) public view returns (bool) {
+        return _partners[partner];
     }
 
-    function setPartnerConfiguration(address partner, PartnerConfiguration memory configuration) public {
-        registrarModule.getAccessControl().isOwner(msg.sender);
-        registrarModule.getAccessControl().isPartner(partner);
-
-        _partnerConfigurations[partner] = configuration;
+    function addPartner(address partner) public onlyOwner {
+        _partners[partner] = true;
     }
 
-    function getPartnerConfiguration(address partner) public returns (PartnerConfiguration memory) {
-        registrarModule.getAccessControl().isPartner(partner);
-
-        return _partnerConfigurations[partner];
+    function removePartner(address partner) public onlyOwner {
+        _partners[partner] = false;
     }
+
+    function setPartnerConfiguration(
+        address partner,
+        PartnerConfiguration memory configuration
+    ) external override {}
+
+    function getPartnerConfiguration(address partner)
+        external
+        override
+        returns (PartnerConfiguration memory)
+    {}
 }
 
