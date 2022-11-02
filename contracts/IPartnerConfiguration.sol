ff8f5d6e7f7443fb71e83c5bee3db0bbd5411d0a
Onyeka Ijeh <onyekaijeh@gmail.com>
2022-11-02 10:49:09 +0100

    feat: partner configuration

---
+++ contracts/IPartnerConfiguration.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.17;
+
+interface IPartnerConfiguration {
+    function getMinLength() external view returns (uint256);
+    function getMaxLength() external view returns (uint256);
+    function getUnicodeSupport() external view returns (bool);
+    function getMinDuration() external view returns (uint256);
+    function getMaxDuration() external view returns (uint256);
+    function getFeePercentage() external view returns (uint256);
+    function getDiscount() external view returns (uint256);
+    function getPrice(string memory name, uint256 duration) external view returns (uint256);
+}
