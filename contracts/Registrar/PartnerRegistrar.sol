e66daf4516e2aac8b72de6909224672b3a1ad012
Onyeka Ijeh <onyekaijeh@gmail.com>
2022-11-10 15:57:56 +0100

    feat: check for commitment when min age is not 0

--- contracts/Registrar/PartnerRegistrar.sol
+++ contracts/Registrar/PartnerRegistrar.sol
@@ -110,10 +110,12 @@ contract PartnerRegistrar is IBaseRegistrar, Ownable {
             "Name too long"
         );
 
-        bytes32 commitment = makeCommitment(label, nameOwner, secret);
-        require(canReveal(commitment), "No commitment found");
-        _commitmentRevealTime[commitment] = 0;
-
+        if (_getPartnerConfiguration().getMinCommittmentAge() != 0) {
+            bytes32 commitment = makeCommitment(label, nameOwner, secret);
+            require(canReveal(commitment), "No commitment found");
+            _commitmentRevealTime[commitment] = 0;
+        }
+        
         _nodeOwner.register(label, nameOwner, duration * 365 days);
 
         return
